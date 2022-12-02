dc9789474651f0fe29fd3fe887ca4049eb1479e7
saipotla123 <114969205+saipotla123@users.noreply.github.com>
2022-12-02 17:30:55 -0600

    Add files via upload

---
+++ HistoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class HistoryModel
+    {
+        public string expression { get; set; }
+    }
+}
